5a5fb4437396487f9917d70b9ebbd865f9cbc1fd
haralampy slavkov <96301516+tmkayy@users.noreply.github.com>
2024-12-03 22:47:04 +0200

    removed obsolete seeder

---
+++ vidya.Data/Seeder/ActivationKeySeeder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vidya.Data.Seeder.Fakers;
+
+namespace vidya.Data.Seeder
+{
+    public class ActivationKeySeeder : ISeeder
+    {
+        public async Task SeedAsync(VidyaDbContext context, IServiceProvider serviceProvider)
+        {
+            if (context.ActivationKeys.Any())
+            {
+                return;
+            }
+            var applicationKeyFaker = new ActivationKeyFaker(context);
+            await context.ActivationKeys.AddRangeAsync(applicationKeyFaker.Generate(16));
+            await context.SaveChangesAsync();
+        }
+    }
+}
